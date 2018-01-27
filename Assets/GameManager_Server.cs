098004e4838eb23225cb92d01c1bec275836a537
Josue Cortina Suarez <jcortina781@gmail.com>
2018-01-27 01:50:56 -0500

    Create the first version of the server Game Manager

---
+++ Assets/GameManager_Server.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+internal enum GameMsgType
+{
+	Movement, Burst
+}
+
+internal class MovementMessage : MessageBase
+{
+	public Vector2 delta;
+}
+
+internal class BurstMessage : MessageBase
+{
+	public KeyCode keyCode;
+}
+
+
+public class GameManager_Server : MonoBehaviour {
+
+	// Use this for initialization
+	private void Start()
+	{
+		NetworkServer.RegisterHandler((short)GameMsgType.Movement, MovementHandler);
+		NetworkServer.RegisterHandler((short)GameMsgType.Burst, BurstHandler);
+	}
+
+	private void MovementHandler(NetworkMessage netMsg)
+	{
+		var movMsg = netMsg.ReadMessage<MovementMessage>();
+
+		var player = netMsg.conn.playerControllers[0].gameObject.GetComponent<PlayerMovement>();
+
+		player.x = movMsg.delta.x;
+		player.y = movMsg.delta.y;
+	}
+
+	private void BurstHandler(NetworkMessage netMsg)
+	{
+		var burstMsg = netMsg.ReadMessage<BurstMessage>();
+
+		var player = netMsg.conn.playerControllers[0].gameObject.GetComponent<PlayerBurst>();
+
+		player.Burst(burstMsg.keyCode);
+	}
+}
