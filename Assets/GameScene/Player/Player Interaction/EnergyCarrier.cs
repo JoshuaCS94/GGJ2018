3657f65412e82fc29daf75baa8bfaa40e735ad5d
Carlos David Muñiz Chall <udl.was.here@gmail.com>
2018-01-27 10:56:04 -0800

    Add Energy layer to proyect

--- Assets/GameScene/Player/Player Interaction/EnergyCarrier.cs
+++ Assets/GameScene/Player/Player Interaction/EnergyCarrier.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnergyCarrier : MonoBehaviour {
+public class EnergyCarrier : MonoBehaviour
+{
+
+	public float Energy = 0;
+
+	public float MaxEnergy = 50;
+	public float EnergyAddedPerSecond = 5;
+
+	public float EnergyUpdateTime = 0.5f;
+
+
 
 	// Use this for initialization
 	void Start () {
-		
+
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.layer == LayerMask.NameToLayer("Energy"))
+			StartCoroutine("AddEnergy");
+
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.layer == LayerMask.NameToLayer("Energy"))
+			StopCoroutine("AddEnergy");
+	}
+
+
+	IEnumerator AddEnergy()
+	{
+		Energy += EnergyAddedPerSecond * EnergyUpdateTime;
+		if (Energy >= MaxEnergy)
+		{
+			Energy = MaxEnergy;
+			yield break;
+		}
+		yield return new WaitForSeconds(EnergyUpdateTime);
 	}
 }
