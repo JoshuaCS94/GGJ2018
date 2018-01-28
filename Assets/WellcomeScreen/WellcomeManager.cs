4d1a51599e1c55a73f783b3a605975d0fc41704e
Manuel Hermida Hadad <manoletecu@gmail.com>
2018-01-28 15:48:41 -0500

    Add wellcome screen

---
+++ Assets/WellcomeScreen/WellcomeManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WellcomeManager : MonoBehaviour
+{
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
