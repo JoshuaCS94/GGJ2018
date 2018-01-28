a18ef791b69938f506879adf41078e9b5ab2742a
Carlos David Muñiz Chall <udl.was.here@gmail.com>
2018-01-28 09:41:22 -0800

    Add animation of hammer

--- Assets/GameScene/Player/Player Interaction/Hammer/HammerInteraction.cs
+++ Assets/GameScene/Player/Player Interaction/Hammer/HammerInteraction.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HammerInteraction : MonoBehaviour {
+public class HammerInteraction : MonoBehaviour
+{
+	public LayerMask Affected;
+
+	private Collider2D Right;
+
+	private Collider2D Left;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+	{
+		var colliders = GetComponents<BoxCollider2D>();
+		Left = colliders[0];
+		Right = colliders[1];
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		var rb = other.gameObject.GetComponent<Rigidbody2D>();
+		if (Left.IsTouchingLayers(Affected))
+		{
+			print("left");
+			var mypos = Left.bounds.center;
+			var playerPos = other.gameObject.transform.position;
+			var direction = Vector3.Normalize(mypos - playerPos);
+
+			print(direction);
+
+			rb.AddForce(direction*-300 + Vector3.left*1000);
+		}
+		else
+		{
+			var mypos = Right.bounds.center;
+//			var contact = other.contacts[points / 2].point;
+			var playerPos = other.bounds.center;
+			var direction = Vector3.Normalize(mypos - playerPos);
+
+			print(direction);
+
+			rb.AddForce(direction*-300 + Vector3.right*1000);
+		}
 	}
 }
