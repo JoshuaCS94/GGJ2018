5376e8165384ea9f3ea1f1192fa2e6844c398e75
Manuel Hermida Hadad <manoletecu@gmail.com>
2018-01-28 17:02:08 -0500

    Add game sounds. Include intro sound

---
+++ Assets/Sounds/IntroMusic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroMusic : MonoBehaviour
+{
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		SceneManager.sceneLoaded += FinishMusic;
+	}
+
+	void FinishMusic(Scene scene, LoadSceneMode mode)
+	{
+		if (scene.buildIndex == 3)
+		{
+			GetComponent<AudioSource>().DOFade(0, 1).OnComplete(() =>
+				SceneManager.sceneLoaded -= FinishMusic).OnComplete(() =>
+				Destroy(gameObject));
+		}
+	}
+}
