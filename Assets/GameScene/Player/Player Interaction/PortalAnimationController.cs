22d8f2bcff4062b85f23096a559022dcc1f37e80
Carlos David Muñiz Chall <udl.was.here@gmail.com>
2018-01-27 17:46:36 -0800

    Add portal animation

---
+++ Assets/GameScene/Player/Player Interaction/PortalAnimationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class PortalAnimationController : MonoBehaviour
+{
+
+	public Transform child;
+
+	private Tweener t;
+	// Use this for initialization
+	void Start ()
+	{
+		child = transform.GetChild(0);
+		t = child.DOLocalMoveY(1.5f, 0.5f).SetLoops(-1,LoopType.Yoyo).Pause();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void StartAnimation()
+	{
+		t.Play();
+	}
+
+	public void StopAnimation()
+	{
+		t.Pause();
+	}
+}
