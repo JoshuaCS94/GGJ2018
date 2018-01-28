d68db92006ec16ff01de13d311c8ea70e5b61014
Manuel Hermida Hadad <manoletecu@gmail.com>
2018-01-27 20:18:13 -0500

    Add player die chance based on energy

--- Assets/GameScene/Player/Player Movement/PlayerCollisions.cs
+++ Assets/GameScene/Player/Player Movement/PlayerCollisions.cs
@@ -6,8 +6,10 @@ public class PlayerCollisions : MonoBehaviour
 {
 	public string PlayerLayerStr;
 	public float ForceMultiplier;
-	public PlayerMovement movement;
-	
+	public PlayerMovement Movement;
+	public EnergyCarrier Energy;
+	public float MaxProbabilityToDie;
+
 	private int PlayerLayer;
 
 	public virtual void Awake()
@@ -15,42 +17,17 @@ public class PlayerCollisions : MonoBehaviour
 		PlayerLayer = LayerMask.NameToLayer(PlayerLayerStr);
 	}
 
-//	public virtual void OnTriggerEnter2D(Collider2D other)
-//	{
-//		if (other.gameObject.layer == PlayerLayer)
-//		{
-//			if (other.gameObject != gameObject && !other.isTrigger)
-//			{
-//				Destroy(other.transform.parent.gameObject);
-//			}
-//		}
-//	}
-
 	private void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.layer != PlayerLayer) return;
-		
-		movement.rb.AddForce(other.relativeVelocity * ForceMultiplier);
-		
-//		var vel = movement.rb.velocity;
-//		var ovel = other.rigidbody.velocity;
-//		if (Mathf.Sign(ovel.x) ==
-//		    Mathf.Sign(vel.x))
-//		{
-//			if (vel.x <= ovel.x)
-//			{
-//				movement.rb.AddForce((ovel - vel) * ForceMultiplier);
-//			}
-//		}
-//
-//		else
-//		{
-//			var absvel = Mathf.Abs(vel.x);
-//			var absovel = Mathf.Abs(ovel.x);
-//			if (absvel <= absovel)
-//			{
-//				movement.rb.AddForce(new Vector2(absovel - absvel, vel.y) * ForceMultiplier);
-//			}
-//		}
+
+		Movement.rb.AddForce(other.relativeVelocity * ForceMultiplier);
+
+		var probability = MaxProbabilityToDie * (Energy.Energy / Energy.MaxEnergy);
+		var random = Random.value;
+		if (random < probability)
+		{
+
+		}
 	}
 }
