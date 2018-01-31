7e20bdf965aeb9f7a79fce6395488b53d539c3d3
Josue Cortina Suarez <jcortina781@gmail.com>
2018-01-30 22:59:03 -0500

    Animate joystick and use the canvas scale on computations

--- Assets/Client/Joystick.cs
+++ Assets/Client/Joystick.cs
@@ -1,34 +1,54 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 
 public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
+	private const float FADE_DURATION = .2f;
+
 	public bool fixedPosition;
+
 	public Vector2 Value { get; private set; }
 
 	private RectTransform m_movArea;
-	private RectTransform m_nipple;
-	private RectTransform m_deadArea;
-
 	private float m_movAreaSize;
+
+	private RectTransform m_deadArea;
 	private float m_deadAreaSizeSqr;
+
+	private RectTransform m_nipple;
+
+	private Sequence m_animation;
 
 	// Use this for initialization
 	private void Start()
 	{
-		m_movArea = transform.GetChild(0) as RectTransform;
-		m_deadArea = transform.GetChild(1) as RectTransform;
-		m_nipple = transform.GetChild(2) as RectTransform;
+		var canvasScale = GameObject.Find("Canvas").transform.localScale;
 
-		m_movAreaSize = m_movArea.sizeDelta.x / 2;
-		var deadAreaSize = m_deadArea.sizeDelta.x / 2;
+		m_movArea = transform.GetChild(0) as RectTransform;
+		m_movAreaSize = m_movArea.rect.width * canvasScale.x / 2;
+
+		m_deadArea = transform.GetChild(1) as RectTransform;
+		var deadAreaSize = m_deadArea.rect.width * canvasScale.x / 2;
 		m_deadAreaSizeSqr = deadAreaSize * deadAreaSize;
 
-		if (fixedPosition)
-		{
+		m_nipple = transform.GetChild(2) as RectTransform;
 
-		}
+		var movAreaImg = m_movArea.GetComponent<Image>();
+		var deadAreaImg = m_deadArea.GetComponent<Image>();
+		var nippleImg = m_nipple.GetComponent<Image>();
+
+		m_animation = DOTween.Sequence()
+			.Join(movAreaImg.DOFade(0, FADE_DURATION))
+			.Join(deadAreaImg.DOFade(0, FADE_DURATION))
+			.Join(nippleImg.DOFade(0, FADE_DURATION))
+			.Pause()
+			.SetAutoKill(false)
+			.SetEase(Ease.Linear);
+
+		m_animation.Complete();
 	}
 
 	public void OnPointerDown(PointerEventData eventData)
@@ -40,12 +60,18 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
 		else
 		{
 			m_movArea.position = m_deadArea.position = m_nipple.position = eventData.position;
+
+			m_animation.PlayBackwards();
 		}
 	}
 
 	public void OnPointerUp(PointerEventData eventData)
 	{
+		m_nipple.position = m_movArea.position;
+
 		Value = Vector2.zero;
+
+		m_animation.PlayForward();
 	}
 
 	public void OnDrag(PointerEventData eventData)
@@ -58,11 +84,18 @@ public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
 
 			var delta = Vector2.ClampMagnitude(eventData.position - movAreaPos, m_movAreaSize);
 
-			if (delta.sqrMagnitude < m_deadAreaSizeSqr) return;
+			if (delta.sqrMagnitude < m_deadAreaSizeSqr)
+			{
+				m_nipple.position = movAreaPos;
 
-			m_nipple.position = movAreaPos + delta;
+				Value = Vector2.zero;
+			}
+			else
+			{
+				m_nipple.position = movAreaPos + delta;
 
-			Value = delta / m_movAreaSize;
+				Value = delta / m_movAreaSize;
+			}
 		}
 	}
 }
